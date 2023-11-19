52cb1c0439dd7946b3e0080d69945c7c395537b7
while( true ){  coding; eat; sleep; } <98078407+Alizubeid@users.noreply.github.com>
2023-11-19 12:09:54 -0500

    Add files via upload

---
+++ installing.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get install build-essential git dkms linux-headers-$(uname -r)
+git clone https://github.com/kelebek333/rtl8188fu
+sudo dkms add ./rtl8188fu
+sudo dkms build rtl8188fu/1.0
+sudo dkms install rtl8188fu/1.0
+sudo cp ./rtl8188fu/firmware/rtl8188fufw.bin /lib/firmware/rtlwifi/
+sudo mkdir -p /etc/modprobe.d/
+sudo touch /etc/modprobe.d/rtl8188fu.conf
+echo "options rtl8188fu rtw_power_mgnt=0 rtw_enusbss=0" | sudo tee /etc/modprobe.d/rtl8188fu.conf
+sudo modprobe -rv rtl8188fu && sudo modprobe -v rtl8188fu
+reboot
